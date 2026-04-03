d86b2f499f0a5a07c81a4cefe02f5b6b17be7e62
Joji <107294025+jojipanackal@users.noreply.github.com>
2026-04-03 11:40:09 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.go
@@ -0,0 +1,34 @@
+func isValidSudoku(board [][]byte) bool {
+	rows := make([]map[byte]bool, 9)
+	cols := make([]map[byte]bool, 9)
+	boxes := make([]map[byte]bool, 9)
+
+	for i := 0; i < 9; i++ {
+		rows[i] = make(map[byte]bool)
+    	cols[i] = make(map[byte]bool)
+    	boxes[i] = make(map[byte]bool)
+	}
+
+	for r:=0; r<9; r++ {
+		for c:=0; c<9; c++ {
+			val := board[r][c]
+			if val == '.' {
+				continue
+			}
+			if cols[c][val] {
+				return false
+			}
+			cols[c][val] = true
+			if rows[r][val] {
+				return false
+			}
+			rows[r][val] = true
+			box := (r/3) * 3 + (c/3)
+			if boxes[box][val] {
+				return false
+			}
+			boxes[box][val] = true
+		}
+	}
+	return true
+}
