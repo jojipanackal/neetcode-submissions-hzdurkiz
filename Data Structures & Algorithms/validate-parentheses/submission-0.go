4e1ac3690b8a0f15b657eacc62210652ed83669e
Joji <107294025+jojipanackal@users.noreply.github.com>
2026-04-04 19:33:48 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.go
@@ -0,0 +1,23 @@
+func isValid(s string) bool {
+    var stack []rune
+
+	for _, ch := range s {
+
+		n := len(stack)
+		if n > 0 {
+			val := stack[n-1]
+			if (val == '(' && ch == ')') || (val == '[' && ch == ']') || (val == '{' && ch == '}') {
+				stack = stack[:n-1]
+				continue
+			}
+		}
+		stack = append(stack, ch)
+	}
+
+	if len(stack) == 0 {
+		return true
+	}
+
+	return false
+
+}
